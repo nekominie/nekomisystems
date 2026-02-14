67f4085b66a367d38fc24f39076602dc0cdee551
nekominie <davidzep77@hotmail.com>
2026-02-14 00:33:26 -0700

    change appsindex ref

--- theos/src/components/data/master_apps_registry.ts
+++ theos/src/components/data/master_apps_registry.ts
@@ -1,4 +1,4 @@
-import { InstalledAppsIndex as UserApps } from '../apps/installedApps/apps_index.ts'
+import { InstalledAppsIndex as UserApps } from '../apps/installedapps/apps_index.ts'
 import { CoreAppsIndex as SystemApps } from '../apps/coreapps/core_apps_index.ts'
 
 export const MasterAppRegistry: Record<string, any> = {
